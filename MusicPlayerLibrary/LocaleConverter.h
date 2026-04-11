b0e91aa0e936e2f16d9e73619e6455a002975281
lucas150670 <lucas150670@petalmail.com>
2026-04-11 18:19:06 +0800

    feat(lyric): integrate ML-based language classification for lrc parsing and improve accuracy

--- MusicPlayerLibrary/LocaleConverter.h
+++ MusicPlayerLibrary/LocaleConverter.h
@@ -9,6 +9,7 @@ namespace MusicPlayerLibrary
 	public:
 		static CStringA GetUtf8StringFromBytesNative(const char* input, size_t size);
 		static CString GetUtf16StringFromUtf8String(const CStringA& input);
+		static std::string GetUtf8StringStdFromUtf16String(const CString& input);
 	};
 
 	public ref class LocaleConverter abstract sealed {
